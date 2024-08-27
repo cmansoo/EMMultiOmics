c1e6d409e4eefeb8771fec8e3375b66dd24015db
cmansoo <cmansoo@naver.com>
2024-08-26 22:09:39 -0400

    to test EMVS.R

--- ignore_dev/main_08252024.R
+++ ignore_dev/main_08252024.R
@@ -1,75 +1,213 @@
 rm(list=ls())
-library(gridExtra)
-library(ggplot2)
-library(glmnet)
-library(MASS)
-library(cBioPortalData)
-library(dplyr)
-library(survival)
-library(coda)
-library(caret)
+# library(gridExtra)
+# library(ggplot2)
+# library(glmnet)
+# library(MASS)
+# library(cBioPortalData)
+# library(dplyr)
+# library(survival)
+# library(coda)
+# library(caret)
 set.seed(2021)
 
-args = commandArgs(trailingOnly=TRUE)
-if (length(args)==0){
-  source('functions.R')
-  mechanistic_mod = 'quadratic'
-  clinic_mod = 'em'
-  # parameters for clinic model
-  local = T
-  dirr = getwd()
-}else{
-  source('functions_final.R')
-  mechanistic_mod = args[1]
-  clinic_mod = args[2]
-  local = F
-  dirr = args[5]
+
+
+### functions
+# copy as local
+`%dopar%` <- foreach::`%dopar%`
+`%do%` <- foreach::`%do%`
+`%<-%` <- future::`%<-%`
+
+# EMVS function
+# what does it return? what is the result? (read Haos paper)
+# M <- what is this matrix for? data matrix? Methylation matrix
+# G <- what is this G?
+# grouping <- ?
+# nu0? nu1? nu? lambda? a? b?
+# can I be a parameter too? choose different values of I?
+
+# clusters for parallel computing
+n_cores <- parallel::detectCores() - 1
+my_cluster <- parallel::makeCluster(
+  n_cores,
+  type="PSOCK"
+)
+
+# register them to be used with %dopar%
+doParallel::registerDoParallel(cl=my_cluster)
+foreach::getDoParRegistered() # TRUE
+foreach::getDoParWorkers() # == n_cores
+
+# run the function
+# load rda file
+load("data/GBM_data2.rda")
+# load GENE_GROUP2
+load("data/GENE_GROUP2.rda")
+
+
+# global vars?
+MECHANISTIC_MOD <- "quadratic"
+CLINIC_MOD <- "em"
+# we are only considering "GBM_DATA2" for now which is for quadratic mechanistic mode?
+# gbm 3 and gbm 1 should be discussed later with Hao as I'm not sure what to do with them
+G <- GBM_data2$G
+C <- GBM_data2$C
+Y <- log(GBM_data2$Y)
+M <- GBM_data2$M
+DELTA <- GBM_data2$Delta
+grouping <- GENE_GROUP2
+
+
+
+#### funciton
+# params
+nu0 <- 0.5
+nu1 <- 10^3
+nu <- 1
+lambda <- 1
+a <- 1
+b <- 1
+
+
+#get dimension of data
+N=nrow(M)
+K=ncol(G)
+J=ncol(M)
+#get functional cluster information
+R = length(unique(grouping))
+Kr = list()
+for (r in unique(grouping)){
+  Kr[[r]]=which(grouping==r)
 }
+#initialize parameters
+estalpha = numeric(R)
+esttau2 = numeric(R)
+estOme=matrix(0,nrow=J,ncol=K)
+estsig2=numeric(K)
+esttheta=numeric(K)
+iteration=numeric(K)
 
-if (local==T){
-  parentPath = getwd()
-  if (mechanistic_mod=='cubic'){
-    data_path = paste0(parentPath,"/GBM_data3.RData")
-  }else if (mechanistic_mod=='quadratic'){
-    data_path = paste0(parentPath,"/GBM_data2.RData")
-  }else{
-    data_path = paste0(parentPath,"/GBM_data1.RData")
+# #### export all the variables to clusters
+# parallel::clusterExport(
+#   cl=my_cluster,
+#   varlist=c("N", "J", "K", "R", "Kr", "estalpha", "esttau2", "estOme", "estsig2", "esttheta", "iteration")
+#   )
+`%dopar%` <- foreach::`%dopar%`
+foreach::foreach(r=1:R) %dopar%{
+  # print(r)
+  cat(r)
+  # I=100
+  I <- 10
+  #initialize estimation
+  ome = thresh_ome = array(0,dim=c(I,J,length(Kr[[r]])))
+  sig2 = array(0,dim=c(I,length(Kr[[r]])))
+  theta = array(0,dim=c(I,length(Kr[[r]])))
+  alpha = array(0,I)
+  tau2 = array(0,I)
+  ome[1,,]=numeric(J)
+  sig2[1,]=1
+  theta[1,]=0.1
+  alpha[1]=0.1
+  tau2[1]=0.1
+  #E-step
+  for (i in 2:I){
+    p=array(0,dim=c(J,length(Kr[[r]])))
+    d=array(0,dim=c(J,length(Kr[[r]])))
+    for (k in 1:length(Kr[[r]])){
+      #initial value of parameters:
+      for (j in 1:J){
+        p1=dnorm(ome[(i-1),j,k],mean=0,sd=sqrt(sig2[i-1,k]*nu1))*theta[i-1,k]
+        p0=dnorm(ome[(i-1),j,k],mean=0,sd=sqrt(sig2[i-1,k]*nu0))*(1-theta[i-1,k])
+        p[j,k]=p1/(p1+p0)
+        d[j,k]=(1-p[j,k])/nu0+p[j,k]/nu1
+      }
+      D=diag(d[,k])
+      #M-step
+      ome[i,,k]=coef(glmnet::glmnet(x=M,y=(G[,Kr[[r]][k]]-alpha[i-1]), intercept = F, standardize = F,
+                            lambda=(sum(d[,k])/ncol(M))/N,alpha=0,penalty.factor = sqrt(d[,k])))[-1,1]
+      # ome[i,,k]=(solve(D)-solve(D)%*%t(M)%*%solve(diag(nrow=N,ncol=N)+M%*%solve(D)%*%t(M))%*%M%*%solve(D))%*%t(M)%*%(G[,Kr[[r]][k]]-alpha[i-1])
+      sig2[i,k]=(t(G[,Kr[[r]][k]]-M%*%ome[i,,k]-alpha[i-1])%*%(G[,Kr[[r]][k]]-M%*%ome[i,,k]-alpha[i-1])+t(sqrt(D)%*%ome[i,,k])%*%(sqrt(D)%*%ome[i,,k])+nu*lambda)/(N+J+nu+2)
+      theta[i,k]=(sum(p[,k])+a-1)/(a+b+J-2)
+    }
+    #thresholding
+    model=matrix(0,nrow=J,ncol=length(Kr[[r]]))
+    for (k in 1:length(Kr[[r]])){
+      postp=numeric(J)
+      for (j in 1:J){
+        p1=dnorm(ome[i,j,k],mean=0,sd=sqrt(sig2[i,k]*nu1))*theta[i,k]
+        p0=dnorm(ome[i,j,k],mean=0,sd=sqrt(sig2[i,k]*nu0))*(1-theta[i,k])
+        postp[j]=p1/(p1+p0)
+        model[j,k]=ifelse(postp[j]>=0.5,1,0)
+        thresh_ome[i,j,k]=ifelse(model[j,k]==1,ome[i,j,k],0)
+      }
+    }
+    tau2[i]=(alpha[i]^2+1)/4
+    alpha[i]=((tau2[i]/sig2[i,])%*%colSums(G[,Kr[[r]]]-M%*%thresh_ome[i,,]))/(1+N*sum((tau2[i]/sig2[i,])))
+    # print('alpha')
+    # print(alpha[i])
+    cat(alpha[i])
+    # print('ome:')
+    # print(sum(abs(thresh_ome[i,,])))
+    # print('----------------')
+    if (sum(abs(ome[i,,]-ome[(i-1),,]))+sum(abs(sig2[i,]-sig2[i-1,]))+sum(abs(theta[i,]-theta[i-1,]))+abs(alpha[i]-alpha[i-1])+abs(tau2[i]-tau2[i-1])<0.0001) {
+      #print('converge')
+      estOme[,Kr[[r]]]=thresh_ome[i,,] 
+      estsig2[Kr[[r]]]=sig2[i,] 
+      esttheta[Kr[[r]]]=theta[i,] 
+      esttau2[r]=tau2[i]
+      estalpha[r]=alpha[i]
+      iteration[Kr[[r]]]=i
+      #print(paste0('alpha',estalpha[r]))
+      break}
   }
-}else{
-  parentPath = "/home/hx222/emvs/"
-  if (mechanistic_mod=='cubic'){
-    data_path = paste0(parentPath,"/GBM_data3.RData")
-  }else if (mechanistic_mod=='quadratic'){
-    data_path = paste0(parentPath,"/GBM_data2.RData")
-  }else{
-    data_path = paste0(parentPath,"/GBM_data1.RData")
-  }
+  estOme[,Kr[[r]]]=thresh_ome[i,,] 
+  estsig2[Kr[[r]]]=sig2[i,] 
+  esttheta[Kr[[r]]]=theta[i,] 
+  esttau2[r]=tau2[i]
+  estalpha[r]=alpha[i]
+  iteration[Kr[[r]]]=i
 }
-# if load subdata, uncomment this trunk
-# load(data_path)
-# Y = log(Y)
-# Delta = myclinical$OS_STATUS
+#estimate random intercept of each functional cluster
+#compute R2 for each gene site
+R2=numeric(K)
+for (kk in 1:K){
+  ind=(estOme[,kk]!=0)
+  R2[kk]=ifelse(sum(ind)==0,0,(summary(lm((G[,kk]-estalpha[grouping[kk]])~M[,ind]-1))$r.squared))
+}
+my_result <- list(estOme=estOme,estsig2=estsig2,esttheta=esttheta,
+            esttau2=esttau2,estalpha=estalpha,
+            iteration=iteration,R2=R2)
 
-# if load full data, uncomment this trunk
-# The loaded data includes gene expression, clinical data, overall survival time, sensorship and gene groupings
-load(data_path)
-G = GBM_data$G
-C = GBM_data$C
-Y = log(GBM_data$Y)
-M = GBM_data$M
-Delta = GBM_data$Delta
+# stop cluster
+# parallel::stopCluster(cl=my_cluster)
 
-#
-#
-#
-#
-# ################################################################################
-# #first-stage
-# Making Grouping matrix Zmatrix
-# run emvs algorithm
-EMVS_res = EMVS(M,G,grouping=gene_group,nu0=0.5,nu1=10^3,nu=1,lambda=1,a=1,b=1)
-save(EMVS_res,file=paste0('GBM_data2_EMVS_res.Rdata'))
-load("GBM_data2_EMVS_res.Rdata")
+# 
+# EMVS = function(M,G,grouping,nu0=0.5,nu1=10^3,nu=1,lambda=1,a=1,b=1){
+#   
+# }
+
+
+
+
+###########################################################
+load("data/GBM_data2_EMVS_res.Rdata")
+
+# What is the expected output format?
+EMVS_res$estOme
+EMVS_res$estsig2
+EMVS_res$esttheta
+EMVS_res$esttau2
+EMVS_res$estalpha
+EMVS_res$iteration
+EMVS_res$R2
+
+##########
+
+
+
+EMVS_res = EMVS(M,G,grouping=GENE_GROUP2,nu0=0.5,nu1=10^3,nu=1,lambda=1,a=1,b=1)
+save(EMVS_res,file=paste0('data/GBM_data2_EMVS_res.Rdata'))
+load("data/GBM_data2_EMVS_res.Rdata")
 N=nrow(G)
 L=ncol(C)
 J=ncol(M)

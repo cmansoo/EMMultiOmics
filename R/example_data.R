90e1d490c902bdb20fbe397417e7751ed5c07f41
cmansoo <cmansoo@naver.com>
2024-08-26 22:37:15 -0400

    test EMVS

---
+++ R/example_data.R
@@ -0,0 +1,19 @@
+#' GBM data 2
+#' @format GBM data 2
+"GBM_data2"
+
+#' GBM full data 2
+#' @format Full data for GBM 2
+"GBM_fulldata2"
+
+#' GBM data 2 EMVS result
+#' @format GBM 2 data EMVS result. Obtained withe GENE_GROUP2
+"GBM_data2_EMVS_res"
+
+#' GBM full data 2 EMVS result
+#' @format GBM 2 full data EMVS result
+"GBM_fulldata2_EMVS_res"
+
+#' GENE GROUPING2 
+#' @format Gene grouping for GBM data 2
+"GENE_GROUP2"
--- test/test_EMVS.R
+++ test/test_EMVS.R
@@ -1,1 +1,12 @@
-# test EMVS
+# setup
+G <- GBM_data2$G
+C <- GBM_data2$C
+Y <- log(GBM_data2$Y)
+M <- GBM_data2$M
+DELTA <- GBM_data2$Delta
+grouping <- GENE_GROUP2
+
+# test 1
+EMVS(M, G, grouping, I=2, THRESH=0.1)
+# test 2
+EMVS_par(M, G, grouping, I=2, THRESH=0.1)

b9a28fabdeb61d78a5db1b9177731992b214075a
cmansoo <cmansoo@naver.com>
2024-08-27 19:06:48 -0400

    test

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' GBM_data2
+#' @format GBM data 2
+"GBM_data2"
+
+#' GENE_GROUP2
+#' @format Gene grouping for GBM data 2
+"GENE_GROUP2"
+
+#' GBM2_EMVS_res
+#' @format GBM 2 data EMVS result. 
+"GBM2_EMVS_res"
+
+#' GBM_data2_full
+#' @format Full data for GBM 2
+"GBM_data2_full"
+
+#' "GENE_GROUP2_FULL"
+#' @format Gene grouping for GBM full data 2
+"GENE_GROUP2_FULL"
+
+#' GBM2_full_EMVS_res 
+#' @format GBM 2 full data EMVS result
+"GBM2_full_EMVS_res"
+
